078f7e1effb7cf7beaa0173160776739abf85349
Santiago Spina <santiagospina025@gmail.com>
2025-04-09 21:40:14 +0200

    Registrar usuario en la bd de Firebase

--- app/src/main/java/com/example/gamebox/LoginActivity.kt
+++ app/src/main/java/com/example/gamebox/LoginActivity.kt
@@ -1,66 +1,84 @@
 package com.example.gamebox
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private var isLogin = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setup();
+    }
 
-        auth = FirebaseAuth.getInstance()
-
+    private fun setup() {
+        //Obtengo todos los elementos
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
         val actionButton = findViewById<Button>(R.id.actionButton)
         val toggleModeText = findViewById<TextView>(R.id.toggleModeText)
         val titleText = findViewById<TextView>(R.id.titleText)
-        val errorText = findViewById<TextView>(R.id.errorText)
+        val errorText = findViewById<TextView>(R.id.errorText);
 
+        //Detecta el click en el boton
         actionButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if (isLogin) {
-                auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            // TODO: ir a pantalla principal
+            //Comprueba que no esten vacios los editText
+            if (emailEditText.text.isEmpty() && passwordEditText.text.isEmpty()) {
+                showAlertDialog(
+                    title = "Campos vacios",
+                    message = "Asegurate de que los campos no esten vacios!"
+                );
+            }
+            //Comprueba la contraseña tenga minimo 6 caracteres
+            else if (passwordEditText.text.length < 6) {
+                showAlertDialog(
+                    title = "Contraseña muy corta",
+                    message = "La contraseña tiene que tener minimo 6 caracteres"
+                )
+            }
+            //Si es correcto, pasa a crear el usuario
+            else {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    )
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show()
+                            showHome() //Mostrar la pantalla de inicio
                         } else {
-                            errorText.text = task.exception?.message
-                            errorText.visibility = View.VISIBLE
-                        }
-                    }
-            } else {
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            // TODO: ir a pantalla principal
-                        } else {
-                            errorText.text = task.exception?.message
-                            errorText.visibility = View.VISIBLE
+                            //Muestra un mensaje de error
+                            showAlertDialog(
+                                title = "Error al registrar",
+                                message = "Ha ocurrido un error al registrar el usuario"
+                            );
                         }
                     }
             }
         }
 
-        toggleModeText.setOnClickListener {
-            isLogin = !isLogin
-            titleText.text = if (isLogin) "Iniciar sesión" else "Registro"
-            actionButton.text = if (isLogin) "Iniciar sesión" else "Registrarse"
-            toggleModeText.text = if (isLogin)
-                "¿No tienes cuenta? Regístrate"
-            else
-                "¿Ya tienes cuenta? Inicia sesión"
-        }
+    }
+
+    //Funcion que muestra un dialog con los campos indicados
+    //(Para no escribir cuatrocientos dialogs distintos)
+    private fun showAlertDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome() {
+        //ACA PONER EL INTENT QUE DIRIGE A LA HOME
     }
 }
