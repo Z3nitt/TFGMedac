01e8351ba70db08f2a74d9c32b7f8089e89861ab
Santiago Spina <santiagospina025@gmail.com>
2025-05-28 10:50:07 +0200

    Fix radio buttons de suscripcion
    Arregle el bug que se seleccionaban todos los radio buttons

--- app/src/main/java/com/example/gamebox/SuscriptionScreenActivity.kt
+++ app/src/main/java/com/example/gamebox/SuscriptionScreenActivity.kt
@@ -3,10 +3,9 @@ package com.example.gamebox
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SuscriptionScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +18,28 @@ class SuscriptionScreenActivity : AppCompatActivity() {
         goback.setOnClickListener {
             startActivity(Intent(this, MainScreenActivity::class.java))
         }
+
+        val rbMonthly: RadioButton  = findViewById(R.id.monthly_plan)
+        val rbYearly: RadioButton  = findViewById(R.id.yearly_plan)
+        val rbLifetime: RadioButton  = findViewById(R.id.lifetime_plan)
+
+        rbMonthly.setOnClickListener{
+            rbMonthly.isChecked = true
+            rbYearly.isChecked = false
+            rbLifetime.isChecked = false
+        }
+
+        rbYearly.setOnClickListener{
+            rbYearly.isChecked = true
+            rbMonthly.isChecked = false
+            rbLifetime.isChecked = false
+        }
+
+        rbLifetime.setOnClickListener{
+            rbLifetime.isChecked = true
+            rbYearly.isChecked = false
+            rbMonthly.isChecked = false
+        }
+
     }
 }
